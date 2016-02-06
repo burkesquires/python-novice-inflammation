58ce478aefb96e77fb90a6aab64a200f9630c917
R. Burke Squires <burkesquires@gmail.com>
2016-02-05 23:57:25 -0500

    Uploading other notebooks

---
+++ calc_circle.py
@@ -0,0 +1,15 @@
+'''Illustrate a global constant being used inside functions.'''
+
+PI = 3.14159265358979   # global constant -- only place the value of PI is set
+
+def circle_area(radius):
+    return PI*radius*radius    # use value of global constant PI
+
+def circle_circumference(radius):
+    return 2*PI*radius         # use value of global constant PI
+
+def main():
+    print('circle area with radius 5:', circle_area(5))
+    print('circumference with radius 5:', circle_circumference(5))
+
+main()
